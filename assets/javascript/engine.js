5723b343454e304b101012cbcb2928023ccdf670
Brian K <karabinchak.brian@gmail.com>
2017-04-24 15:42:15 -0400

    Added error messages and validating input, started full run function

--- assets/javascript/engine.js
+++ assets/javascript/engine.js
@@ -1,20 +1,15 @@
+var income=0, deductions=0, filingStatusSelected='';
+var incomeTax;
+
 var exampleBreakdown = {
-	'Defense':.60;
-	'Education':.20;
-	'Welfase':.10;
+	'Defense':.60,
+	'Education':.20,
+	'Welfase':.10,
 }
 
-var realUSBreakdow = {
+var realUSBreakdown = {
 	//
 }
-
-function runResults(){
-	console.log('Woo, running!');
-	$('userResults').append('clicked!');
-}
-
-$('#runResults').on('click', runResults());
-$('#userResults').on('click', runResults());
 
 function calculateTax(income, filingStatus) {
 	switch(filingStatus) {
@@ -88,6 +83,68 @@ function checkInput(input){
 }
 
 function returnTaxBreakdown(taxPaid, departmentBreakdown) {
+	console.log('returnTaxBd');
 	//basically loop through department breakdown, spit out department name + taxPaid * department%
 	//return 2-D loop [ [department1, $320], [department2, $423], etc ] 
 }
+
+
+function errorCheck(status, inc, ded) {
+	console.log('Running error check');
+	//return false if there is an error in the data, otherwise return true
+	if(status==='') {
+		$('#errorMessage').html('Please select your filing status');
+		return false;
+	} else if (income.length==0) {
+		console.log('income is not a number');
+		$('#errorMessage').html('Please enter a number for income');
+		return false;	
+	} else if (income <= 0) {
+		$('#errorMessage').html('Please enter a POSITIVE number for income');
+		return false;
+	} else if (deductions.length==0) {
+		console.log('deductions is not a number');
+		$('#errorMessage').html('Please enter a number for deductions');
+		return false;	
+	} else if (deductions <= 0) {
+		$('#errorMessage').html('Please enter deductions as a POSITIVE number');
+		return false;	
+	} else {
+		$('#errorMessage').html('');
+		return true;
+	}
+}
+
+function runTheNumbers() {
+	income = $('#incomeInput').val();
+	deductions = $('#deductionsInput').val();
+
+	if (errorCheck(filingStatusSelected, income, deductions)==false) {
+		//error Check has error messages built in, no need to do anything here
+	} else {
+		//everything entered correctly, filing status income deductions
+		//turn income/deductions into numbers
+		income=parseFloat(income);
+		deductions=parseFloat(deductions);
+		//calculate actual tax paid
+		incomeTax=calculateTax(income-deductions, filingStatusSelected);
+		console.log(incomeTax);
+		//present basic findings
+		presentResults(income, deductions, incomeTax);
+	}
+}
+
+function presentResults(inc, ded, incTax) {
+	
+}
+
+$(document).ready( function() {
+	console.log('Doc is ready!');
+	//Getting Filing STatus
+	$("input:radio[name=filingStatus]").click(function() {
+    filingStatusSelected= $(this).val();
+   	console.log(filingStatusSelected);
+	});
+	//Get numbers
+	$('#runResults').on('click', runTheNumbers);
+});
